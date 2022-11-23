d88d654aed4a76be8bb30f944406e9031ae52974
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2022-11-23 14:01:10 +0900

    不要インポート削除

--- app/src/main/java/jp/example/view_pager_2_infinite_scroll/NumberAdapter.kt
+++ app/src/main/java/jp/example/view_pager_2_infinite_scroll/NumberAdapter.kt
@@ -1,14 +1,8 @@
 package jp.example.view_pager_2_infinite_scroll
 
-import android.content.Context
-import android.graphics.Color
-import android.view.LayoutInflater
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
-import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import jp.example.view_pager_2_infinite_scroll.databinding.AdapterNumberBinding
 
 class NumberAdapterForFragment(
     fragment: FragmentActivity,
